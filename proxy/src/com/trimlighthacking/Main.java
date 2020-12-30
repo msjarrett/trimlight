4bce5e1ff0234f7960c2a8ae2036684e77443247
Stephen Horne <VertigoTwo@live.com>
2020-12-30 12:04:55 -0800

    test proxy

--- proxy/src/com/trimlighthacking/Main.java
+++ proxy/src/com/trimlighthacking/Main.java
@@ -1,9 +1,20 @@
 package com.trimlighthacking;
 
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello world");
+
+        Server s = new Server(System.out, 12); // log stream, max connections
+        try {
+            s.addService(new TrimlightProxyServer("192.168.254.51", 22), 20022);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
