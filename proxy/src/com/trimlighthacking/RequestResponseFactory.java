5512bc101b716d51520b34fe229686ebf6dac836
Stephen Horne <VertigoTwo@live.com>
2020-12-31 04:05:27 -0800

    Simplify

--- proxy/src/com/trimlighthacking/RequestResponseFactory.java
+++ proxy/src/com/trimlighthacking/RequestResponseFactory.java
@@ -3,48 +3,45 @@ package com.trimlighthacking;
 import java.util.Arrays;
 
 public class RequestResponseFactory {
-    private static String ExpectedResponse;
-    private static final Object LOCK = new Object() {};
-    private static void setExpectedResponse(String expectedResponse) {
+    private static byte previousRequestCode;
+    private static final Object LOCK = new Object() {
+    };
+
+    private static void setPreviousRequestCode(byte currentRequestCode) {
         synchronized (LOCK) {
-            ExpectedResponse = expectedResponse;
+            RequestResponseFactory.previousRequestCode = currentRequestCode;
         }
     }
+
     public static TrimlightInterpretable Create(byte[] buffer, int bytes_read, boolean toServer) {
-        if(buffer.length < 3) {
+        if (buffer.length < 3) {
             throw new MalformedBufferException("Buffer should be more than three characters long", buffer);
         }
 
         byte[] bufferSlice = Arrays.copyOfRange(buffer, 0, bytes_read);
 
-        if((bufferSlice[0] & 0xFF) != 0x5A || (bufferSlice[bufferSlice.length - 1] & 0xFF) != 0xA5) {
-            throw new MalformedBufferException(String.format("Buffer is not bookended by 0x5A and 0xA5. Instead, it has %02X and %02X", bufferSlice[0], bufferSlice[bufferSlice.length -1]), bufferSlice);
+        if ((bufferSlice[0] & 0xFF) != 0x5A || (bufferSlice[bufferSlice.length - 1] & 0xFF) != 0xA5) {
+            throw new MalformedBufferException(String.format("Buffer is not bookended by 0x5A and 0xA5. Instead, it has %02X and %02X", bufferSlice[0], bufferSlice[bufferSlice.length - 1]), bufferSlice);
         }
 
         byte code = buffer[1];
-
-        if(toServer) {
+        if (toServer) {
+            setPreviousRequestCode(code);
             switch (code & 0xFF) {
                 case 0x0C:
-                    setExpectedResponse("ConnectionResponse");
+
                     return new ConnectionRequest(bufferSlice);
                 default:
-                    setExpectedResponse(null);
                     return new UnclassifiedRequest(bufferSlice);
             }
         } else {
-            try {
-                switch (ExpectedResponse) {
-                    case "ConnectionResponse":
-                        return new ConnectionResponse(bufferSlice);
-                }
-                switch (code) {
-                    default:
-                        return new UnclassifiedResponse(bufferSlice);
-                }
+            switch (previousRequestCode & 0xFF) {
+                case 0x0C:
+                    return new ConnectionResponse(bufferSlice);
             }
-            finally {
-                setExpectedResponse(null);
+            switch (code) {
+                default:
+                    return new UnclassifiedResponse(bufferSlice);
             }
         }
     }
